88098343b42e437b74b44809d5358c00d059f0d2
araxielfenix <araxielfenix@outlook.com>
2023-04-02 02:43:09 -0600

    se cambio la url

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     port: 3000,
   },
-  base: '/Anaizador-Solidjs/',
+  base: 'https://araxielfenix.github.io/Analizador-Solidjs/',
   build: {
     target: 'esnext',
   },
